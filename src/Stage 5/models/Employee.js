b2a066e8f67ac66a707a2621a7d5d94f13791455
Ajaalexandra <ajacederberg@gmail.com>
2017-10-16 22:33:18 -0500

    Committed employee-manager

--- src/Stage 5/models/Employee.js
+++ src/Stage 5/models/Employee.js
@@ -1,9 +1,23 @@
 export default class Employee {
-  // constructor
-  
-  // updateName
+  constructor(id, name, phone, title){
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.title = title;
+  }// constructor
 
-  // updatePhone
+  updateName(name) {
+    this.name = name;
+  }// updateName
 
-   // updateTitle
+
+updatePhone(phone) {
+  this.phone = phone;
+}
+// updatePhone
+
+updateTitle(title) {
+  this.title = title;
+}   // updateTitle
+
 }
--- src/Stage 8/components/EmployeeList.js
+++ src/Stage 8/components/EmployeeList.js
@@ -5,16 +5,18 @@ class EmployeeList extends Component {
     return (
       <div>
         <ul className="listContainer">
-          { 
-            this.props.employees.map((employee) => {
-              return (
-                <li className="listText" key={employee.id} onClick={ () => { this.props.selectEmployee(employee) }}> { employee.name } </li>
-              )
-            })
-          }
+          {this.props.employees.map(cur => (
+            <li
+              key={cur.id}
+              className="listText"
+              onClick={() => this.props.selectEmployee(cur)}
+            >
+              {cur.name}
+            </li>
+          ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
